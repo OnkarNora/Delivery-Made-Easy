456ecfa0b9e1510fc30941a1a6ff7a6bb1f8012a
OnkarNora <68858889+OnkarNora@users.noreply.github.com>
2024-04-10 21:10:42 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -17,14 +17,14 @@ import {
     addDoc,
 } from "firebase/firestore";
 const firebaseConfig = {
-    apiKey: "AIzaSyACpZdE4Ff2XLZg2TlkCJwQkq-rSDk_jFA",
-    authDomain: "delivery-made-easy.firebaseapp.com",
-    databaseURL: "https://delivery-made-easy-default-rtdb.firebaseio.com",
-    projectId: "delivery-made-easy",
-    storageBucket: "delivery-made-easy.appspot.com",
-    messagingSenderId: "528491488333",
-    appId: "1:528491488333:web:27c5eb956af9ef80456804",
-    measurementId: "G-F89RMJDFVW"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
